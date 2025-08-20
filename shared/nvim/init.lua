210380a9182eb9d89e3da7504ee12e1358d04339
Nolan Miller <nolan.miller77@gmail.com>
2025-08-20 06:10:17 -0400

    update harpoon mapping nvim

--- shared/nvim/init.lua
+++ shared/nvim/init.lua
@@ -1220,7 +1220,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<C-l>', function()
         harpoon:list():select(3)
       end)
-      vim.keymap.set('n', '<C-;>', function()
+      vim.keymap.set('n', '<C-h>', function()
         harpoon:list():select(4)
       end)
 
