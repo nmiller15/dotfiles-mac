41018e4ca1798f08742742359d91f63e2feee6ec
Nolan Miller <nolan.miller77@gmail.com>
2025-10-31 20:54:09 -0400

    add various mac/nvim configs

--- mac/shell/aliases.sh
+++ mac/shell/aliases.sh
@@ -18,7 +18,7 @@ alias ga='git add .'
 # util
 alias reload=". ~/Projects/dotfiles/bootstrap/bootstrap.mac.zsh"
 alias pushblog="/Users/nolanmiller/Projects/log-nolan/scripts/pushblog.sh" # Add this to the path
-alias cslox="/Users/nolanmiller/Projects/cslox/cslox.App/bin/Debug/net9.0/cslox"
+# alias cslox="/Users/nolanmiller/Projects/cslox/cslox.App/bin/Debug/net9.0/cslox"
 alias loxgen="/Users/nolanmiller/Projects/cslox/cslox.GenerateAst/bin/Debug/net9.0/GenerateAst /Users/nolanmiller/Projects/cslox/cslox.App/Models"
 
 # Env variables
